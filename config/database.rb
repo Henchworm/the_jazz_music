8dcb4fec195dea7eb7c9cf58bcffefd135943855
Chris Hewitt <agop5134@gmail.com>
2022-01-21 14:23:29 -0700

    change config

--- config/database.rb
+++ config/database.rb
@@ -4,11 +4,6 @@ database_name = "the-jazz-music-#{TheJazzMusicApp.environment}"
 db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost#{datbase_name}")
 
 ActiveRecord::Base.establish_connection(
- :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
- :host => db.host,
- :port => db.port,
- :username => db.user,
- :password => db.password,
- :database => "#{database_name}",
- :encoding => 'utf8'
+  :adapter  => "postgresql",
+  :database => "db/#{database_name}",
 )
