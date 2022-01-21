d8e406e8f6fa227b7f0d317b68560a297f86d139
Chris Hewitt <agop5134@gmail.com>
2022-01-21 14:19:50 -0700

    Add config

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,19 @@
 require 'bundler'
 Bundler.require
 
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/the-jazz-music')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+
 APP_ROOT = File.expand_path("..", __dir__)
 # require the controller(s)
 Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
