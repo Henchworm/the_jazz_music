2d8dbf78d052faa524042845cff2cbb3a5f07fea
Chris Hewitt <agop5134@gmail.com>
2022-01-23 13:02:23 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ gig_1 = Gig.create!(
   date: Time.new(2022, 10, 31),
   band_name:
   "Trains and Dreams",
-  music_link: "fakemusic.com")
+  deets: "Big ones only",
+  venue: "Lion's Lair",
+  music_link: "http://chrishewitt.bandcamp.com")
