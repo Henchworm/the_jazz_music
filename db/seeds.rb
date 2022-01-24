79e56c535e87128f46d3d89b7b602fb84b8093fd
Chris Hewitt <agop5134@gmail.com>
2022-01-24 16:03:21 -0700

    Formatting, blarg seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-gig_1 = Gig.create!(
-  date: Time.new(2022, 10, 31),
-  band_name:
-  "Trains and Dreams",
-  deets: "Big ones only",
-  venue: "Lion's Lair",
-  music_link: "http://chrishewitt.bandcamp.com",
-)
+
+
+blarg_1 = Blarg.create!(text: "Hey there hot lips, thanks for visiting my page. I built it in Sinatra with ActiveRecord/Postgres extensions, more about that on my GitHub(hit the back button and smash the GH link).
+I'll be promoting all my gigs, projects and music on here are I continue to build out the site.
+In other news: the Denver Taco Bell holocaust is upon us. Worker shortages are making it near impossible to dine at America's favorite Mexican restaurant.
+In Denver, AVOID the following Taco Bell Locations:
+Federal & 52, 38th and Irving, Sloan's Lake.
+The Arvada Taco Bell near the Wadsworth Bypass is functioning nominally.
+LOL(lots of love!),
+Golden Bullfrog", title: "Hi, and Taco Bell", subject: "Fine Dining")
